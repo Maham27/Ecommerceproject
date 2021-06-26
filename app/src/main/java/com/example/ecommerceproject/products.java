f402c3a6c7ae0fbf337ab472aceaa91cc41d9fce
Maham27 <mahamnaveed482@gmail.com>
2021-06-26 14:51:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ecommerceproject/products.java
@@ -0,0 +1,41 @@
+package com.example.ecommerceproject;
+
+public class products {
+
+    String name;
+    String image;
+    int price;
+
+    public products() {
+    }
+
+    public products(String name,String image,int price) {
+        this.name=name;
+        this.image=image;
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
