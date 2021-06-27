dbef7a1f6e4a751d521fc304a098c2912816f72b
Maham27 <mahamnaveed482@gmail.com>
2021-06-27 13:24:14 +0500

    init

--- app/src/main/java/com/example/ecommerceproject/informationactivity.java
+++ app/src/main/java/com/example/ecommerceproject/informationactivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,50 +25,45 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 
+import static android.content.ContentValues.TAG;
+
 public class informationactivity extends AppCompatActivity {
     ImageView iminfo;
-    TextView tvnameinfo,tvpriceinfo;
+    TextView tvnameinfo, tvpriceinfo;
     private FirebaseFirestore fc;
     FirebaseAuth auth;
-    products p=null;
-    allproducts s=null;
-    Button cart,buy;
+    products p = null;
+    allproducts s = null;
+    Button cart, buy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informationactivity);
-        iminfo=findViewById(R.id.iminfo);
-        tvnameinfo=findViewById(R.id.tvnameinfo);
-        tvpriceinfo=findViewById(R.id.tvpriceinfo);
-        cart=findViewById(R.id.cart);
-        buy=findViewById(R.id.buy);
+        iminfo = findViewById(R.id.iminfo);
+        tvnameinfo = findViewById(R.id.tvnameinfo);
+        tvpriceinfo = findViewById(R.id.tvpriceinfo);
+        cart = findViewById(R.id.cart);
+        buy = findViewById(R.id.buy);
 
-        fc=FirebaseFirestore.getInstance();
-        auth=FirebaseAuth.getInstance();
-        final Object o=getIntent().getSerializableExtra("information");
-        if(o instanceof products)
+        fc = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        final Object o = getIntent().getSerializableExtra("information");
+        if (o instanceof products) {
+            p = (products) o;
 
-        {
-            p=(products) o;
-
-        }
-        else if(o instanceof allproducts)
-
-        {
-            s=(allproducts) o;
+        } else if (o instanceof allproducts) {
+            s = (allproducts) o;
 
         }
 
-        if(p!=null)
-        {
+        if (p != null) {
             Glide.with(getApplicationContext()).load(p.getImage()).into(iminfo);
             tvnameinfo.setText(p.getName());
             tvpriceinfo.setText(String.valueOf(p.getPrice()));
         }
 
-        if(s!=null)
-        {
+        if (s != null) {
             Glide.with(getApplicationContext()).load(s.getI()).into(iminfo);
             tvnameinfo.setText(s.getN());
             tvpriceinfo.setText(String.valueOf(s.getP()));
@@ -75,38 +72,60 @@ public class informationactivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 cart();
+                //Toast.makeText(informationactivity.this, ""+tvnameinfo.getText(), Toast.LENGTH_SHORT).show();
             }
 
 
         });
 
-}
+    }
 
     private void cart() {
-        String ctime,cdate;
-        Calendar  calendar=Calendar.getInstance();
-        SimpleDateFormat cd=new SimpleDateFormat("MM dd,yyyy");
-                cdate=cd.format(calendar.getTime());
-        SimpleDateFormat ct=new SimpleDateFormat("HH:mm:ss a");
-        ctime=ct.format(calendar.getTime());
-        final HashMap<String,Object> cartmap=new HashMap<>();
-        cartmap.put("productname",tvnameinfo.getText().toString());
-        cartmap.put("productprice",tvpriceinfo.getText().toString());
-        cartmap.put("currenttime",ctime);
-        cartmap.put("currentdate",cdate);
-        fc.collection("addtocart").document(auth.getCurrentUser().getUid()).collection("user")
-                .add(cartmap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                Toast.makeText(informationactivity.this, "it is added to cart", Toast.LENGTH_SHORT).show();
-                finish();
-            }
+        String ctime, cdate;
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat cd = new SimpleDateFormat("MM dd,yyyy");
+        cdate = cd.format(calendar.getTime());
+        SimpleDateFormat ct = new SimpleDateFormat("HH:mm:ss a");
+        ctime = ct.format(calendar.getTime());
+        final HashMap<String, Object> cartmap = new HashMap<>();
+        cartmap.put("productname", tvnameinfo.getText().toString());
+        cartmap.put("productprice", tvpriceinfo.getText().toString());
+        cartmap.put("currenttime", ctime);
+        cartmap.put("currentdate", cdate);
+        //fc.collection("addtocart").document(auth.getCurrentUser().getUid()).collection("User")
+        //  .add(cartmap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+        //@Override
+        //public void onComplete(@NonNull Task<DocumentReference> task) {
 
-       });
+        //    Toast.makeText(informationactivity.this, "it is added to cart", Toast.LENGTH_SHORT).show();
+        //      finish();
 
 
+        //}
+        fc.collection("users")
+                .add(cartmap)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
 
+                        Toast.makeText(informationactivity.this, "it is added to cart", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+
+                    }
+                });
 
 
     }
-    }
+}
+
+
+
+
+
+
